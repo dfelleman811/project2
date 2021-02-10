d6899c36135f792646b7734722c94f0eb3cf5a47
phil <62857839+dfelleman811@users.noreply.github.com>
2021-02-10 18:33:57 -0500

    adding repo, services for item to test

---
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/services/ItemServices.java
@@ -0,0 +1,8 @@
+package com.excape.services;
+
+import com.excape.beans.Item;
+
+public interface ItemServices {
+
+	public Item addItem(Item i);
+}
---
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/services/ItemServicesImpl.java
@@ -0,0 +1,13 @@
+package com.excape.services;
+
+import com.excape.beans.Item;
+
+public class ItemServicesImpl implements ItemServices {
+
+	@Override
+	public Item addItem(Item i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
