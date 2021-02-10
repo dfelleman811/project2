f192ee9e7aff5f1f28a8d75b4c9725044bd81478
phil <62857839+dfelleman811@users.noreply.github.com>
2021-02-09 19:18:54 -0500

    creating Spring Project folder/package structure

---
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/app/Project2EscapeRoomProductAppApplication.java
@@ -0,0 +1,19 @@
+package com.excape.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan("com.excape") // Annotation to provide location for packages
+@EnableJpaRepositories("com.excape.repositories") // Annotation specifically for Repo resource location
+@EntityScan("com.excape.beans") // Annotation to tell it where our entities are
+public class Project2EscapeRoomProductAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2EscapeRoomProductAppApplication.class, args);
+	}
+
+}
