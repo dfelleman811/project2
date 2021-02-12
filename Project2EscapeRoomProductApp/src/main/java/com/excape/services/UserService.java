0f146ac192674e294fa0ddb2c8ea65f85a002c04
bhsimon97 <76971018+bhsimon97@users.noreply.github.com>
2021-02-11 19:12:27 -0500

    user crud operations

---
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/services/UserService.java
@@ -0,0 +1,17 @@
+package com.excape.services;
+
+import java.util.List;
+
+import com.excape.beans.User;
+
+public interface UserService {
+	
+	public User getUser(int id);
+	public List<User> getAllUsers();
+	public List<User> getUsersByLastName(String lastName);
+	public List<User> getUsersByBusinessName(String businessName);
+	public User addUser(User u);
+	public User updateUser(User u);
+	public boolean deleteUser(int id);
+
+}
---
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/services/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.excape.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.excape.beans.User;
+import com.excape.repositories.UserRepo;
+
+@Service
+public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	UserRepo ur;
+	
+	@Override
+	public User addUser(User u) {
+		ur.save(u);
+		return u;
+	}
+	
+	@Override
+	public List<User> getAllUsers(){
+		return (List<User>) ur.findAll();
+	}
+
+	@Override
+	public User getUser(int id) {
+		return ur.findById(id).get();
+	}
+
+	@Override
+	public List<User> getUsersByLastName(String lastName) {
+		return (List<User>) ur.findByLastName(lastName);
+	}
+
+	@Override
+	public List<User> getUsersByBusinessName(String businessName) {
+		return (List<User>) ur.findByBusinessName(businessName);
+	}
+
+	@Override
+	public User updateUser(User u) {
+		return ur.save(u);
+	}
+
+	@Override
+	public boolean deleteUser(int id) {
+		ur.delete(ur.findById(id).get());
+		return true;
+	}
+	
+	
+
+}
