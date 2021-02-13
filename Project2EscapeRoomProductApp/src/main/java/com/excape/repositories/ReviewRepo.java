606c33be86a6d533696e0ce47bb124ea956c16f5
phil <62857839+dfelleman811@users.noreply.github.com>
2021-02-13 08:59:56 -0500

    finishing Review Controller

--- Project2EscapeRoomProductApp/src/main/java/com/excape/repositories/ReviewRepo.java
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/repositories/ReviewRepo.java
@@ -1,8 +1,15 @@
 package com.excape.repositories;
 
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ReviewRepo {
+import com.excape.beans.Review;
 
+@Repository
+public interface ReviewRepo extends CrudRepository<Review, Integer> {
+
+	public List<Review> findByUserRating(int userRating);
+	public List<Review> findByUserDifficulty(int userDiffRating);
 }
--- Project2EscapeRoomProductApp/src/main/java/com/excape/services/ReviewServicesImpl.java
+++ Project2EscapeRoomProductApp/src/main/java/com/excape/services/ReviewServicesImpl.java
@@ -2,53 +2,57 @@ package com.excape.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.excape.beans.Review;
+import com.excape.repositories.ReviewRepo;
 
 @Service
 public class ReviewServicesImpl implements ReviewServices {
+	
+	@Autowired
+	private ReviewRepo rr;
 
 	@Override
 	public Review getReview(int reviewId) {
-		// TODO Auto-generated method stub
-		return null;
+		return rr.findById(reviewId).get();
 	}
 
 	@Override
 	public List<Review> getReviewsByItem(int itemId) {
-		// TODO Auto-generated method stub
-		return null;
+		return (List<Review>) rr.findAll();
 	}
 
 	@Override
 	public List<Review> getReviewsByRating(int userRating) {
-		// TODO Auto-generated method stub
-		return null;
+		return rr.findByUserRating(userRating);
 	}
 
 	@Override
 	public List<Review> getReviewsByDifficulty(int userDiffRating) {
-		// TODO Auto-generated method stub
-		return null;
+		return rr.findByUserDifficulty(userDiffRating);
 	}
 
 	@Override
 	public Review addReview(Review r) {
-		// TODO Auto-generated method stub
-		return null;
+		return rr.save(r);
 	}
 
 	@Override
 	public Review updateReview(Review rChange) {
-		// TODO Auto-generated method stub
-		return null;
+		return rr.save(rChange);
 	}
 
 	@Override
 	public boolean deleteReview(int reviewId) {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			rr.delete(rr.findById(reviewId).get());
+			return true;
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 }
